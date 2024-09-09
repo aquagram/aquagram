e2cf6420876f782d8adf78fab22783af7e4312a8
GHOSTsama2503 <87672676+GHOSTsama2503@users.noreply.github.com>
2024-09-09 01:55:14 -0400

    fix: restored album.go content from previous commit

--- album.go
+++ album.go
@@ -1,54 +1,91 @@
 package aquagram
 
 import (
-	"encoding/json"
-	"errors"
+	"context"
+	"fmt"
 	"strconv"
-	"strings"
 )
 
-const (
-	EmptyString   string = ""
-	TrueAsString  string = "true"
-	FalseAsString string = "false"
-)
-
-func ParseRawResult[T any](bot *Bot, data []byte) (T, error) {
-	var res struct {
-		Ok          bool   `json:"ok"`
-		Result      T      `json:"result"`
-		ErrorCode   int    `json:"error_code"`
-		Description string `json:"description"`
-	}
-
-	if err := json.Unmarshal(data, &res); err != nil {
-		return res.Result, err
-	}
-
-	if !res.Ok {
-		if res.ErrorCode != 0 {
-			return res.Result, errTgBadRequest(res.ErrorCode, res.Description)
-		}
-
-		return res.Result, errors.New("unknown error parsing raw result: " + string(data))
-	}
-
-	if message, ok := any(&res.Result).(*Message); ok {
-		message.process(bot)
-	}
-
-	return res.Result, nil
+type SendMediaGroupParams struct {
+	BusinessConnectionID string           `json:"business_connection_id,omitempty"`
+	ChatID               string           `json:"chat_id"`
+	MessageThreadID      int              `json:"message_thread_id,omitempty"`
+	Media                []*InputFile     `json:"media"`
+	DisableNotification  bool             `json:"disable_notification,omitempty"`
+	ProtectContent       bool             `json:"protect_content,omitempty"`
+	MessageEffectID      string           `json:"message_effect_id,omitempty"`
+	ReplyParameters      *ReplyParameters `json:"reply_parameters,omitempty"`
 }
 
-func ParseChatID(chatID string) string {
-	at := "@"
+func (bot *Bot) SendMediaGroup(chatID string, media MediaGroup, params *SendMediaGroupParams) ([]Message, error) {
+	return bot.SendMediaGroupWithContext(bot.stopContext, chatID, media, params)
+}
 
-	if !strings.HasPrefix(chatID, at) {
-		_, err := strconv.ParseInt(chatID, 0, 0)
-		if err != nil {
-			chatID = at + chatID
-		}
+func (bot *Bot) SendMediaGroupWithContext(ctx context.Context, chatID string, media MediaGroup, params *SendMediaGroupParams) ([]Message, error) {
+	if params == nil {
+		params = new(SendMediaGroupParams)
 	}
 
-	return chatID
+	files := make(Files)
+	mediaFiles := make([]Params, 0)
+
+	for index, item := range media {
+		itemParams := item.GetParams()
+
+		itemParamsMap, err := itemParams.ToParams()
+		if err != nil {
+			return nil, err
+		}
+
+		itemMedia := item.GetMedia()
+		fieldname := strconv.Itoa(index)
+
+		if itemMedia.FromReader != nil || itemMedia.FromPath != EmptyString {
+			itemParamsMap["media"] = fmt.Sprintf("attach://%s", fieldname)
+			files[fieldname] = itemMedia
+
+		} else if str := itemMedia.FromFileID; str != EmptyString {
+			itemParamsMap["media"] = str
+
+		} else if str := itemMedia.FromURL; str != EmptyString {
+			itemParamsMap["media"] = str
+
+		} else {
+			return nil, ErrUnknownFileSource
+		}
+
+		mediaFiles = append(mediaFiles, itemParamsMap)
+	}
+
+	sendParams := CommonSendParams{
+		BusinessConnectionID: params.BusinessConnectionID,
+		ChatID:               chatID,
+		Media:                mediaFiles,
+		MessageThreadID:      params.MessageThreadID,
+		DisableNotification:  params.DisableNotification,
+		ProtectContent:       params.ProtectContent,
+		MessageEffectID:      params.MessageEffectID,
+		ReplyParameters:      params.ReplyParameters,
+	}
+
+	paramsMap, err := sendParams.ToParams()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(files) == 0 {
+		data, err := bot.Raw(ctx, "sendMediaGroup", paramsMap)
+		if err != nil {
+			return nil, err
+		}
+
+		return ParseRawResult[[]Message](bot, data)
+	}
+
+	data, err := bot.RawFile(ctx, "sendMediaGroup", paramsMap, files)
+	if err != nil {
+		return nil, err
+	}
+
+	return ParseRawResult[[]Message](bot, data)
 }
